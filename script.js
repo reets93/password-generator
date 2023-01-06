6bd25c533e5587f222bc1db60f55a274eba15f0c
reets <117296082+reets93@users.noreply.github.com>
2023-01-06 19:37:42 +0000

    initial prompts and alerts added

--- script.js
+++ script.js
@@ -90,19 +90,32 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-
+// prompt for number of characters
+prompt("How many characters long would you like your password to be? /n It should be between 11 and 64 characters.")
+// alert for include: lower case
+alert("Would you like to use lower case characters? \n Ok for yes.")
+//alert for include: upper case
+alert("Would you like to use upper case characters? \n Ok for yes.")
+// alert for include: special characters
+alert("Would you like to use special characters? \n Ok for yes.")
+// alert for include: numbers
+alert("Would you like to use numeric characters? \n Ok for yes.")
 }
+
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+// limit the randomised generator based on answers from prompts above. 
 }
 
 // Function to generate password with user input
 function generatePassword() {
-
+getPasswordOptions()
+return "Here's your password."
 }
 
+
+// DO NOT MAKE CHANGES TO BELOW:
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
 
