a2901aef9c5288bcceefe5651110182c82c109a3
reets <117296082+reets93@users.noreply.github.com>
2023-01-10 15:08:43 +0000

    solved the code. functioning now and generating correctly

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 // Array of special characters to be included in password
 var specialCharacters = [
   '@',
@@ -24,10 +25,8 @@ var specialCharacters = [
   '_',
   '.'
 ];
-
 // Array of numeric characters to be included in password
 var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
 // Array of lowercase characters to be included in password
 var lowerCasedCharacters = [
   'a',
@@ -57,7 +56,6 @@ var lowerCasedCharacters = [
   'y',
   'z'
 ];
-
 // Array of uppercase characters to be included in password
 var upperCasedCharacters = [
   'A',
@@ -87,88 +85,97 @@ var upperCasedCharacters = [
   'Y',
   'Z'
 ];
-
 var pwCriteria = [];
 var pwLength;
-
+// Function for getting a random element from an array
+function getRandom(arr) {
+  var randIndex = Math.floor(Math.random() * arr.length);
+  var randElement = arr[randIndex];
+  return randElement;
+}
 // // Function to prompt user for password options & Validate answers
 function getPasswordOptions() {
   // Password length + validation
-  pwLength = prompt("How many characters would you like in your password? \n It must be between 10 - 64 characters long.");
-  while (pwLength < 10 || pwLength > 64) {
-    pwLength = prompt("Your password needs between 10 & 64 characters. \n How many characters would you like in your password?");
+  pwLength = prompt("How many characters would you like in your password? \n It must be between 10-64 characters long.");
+  if (pwLength < 10 || pwLength > 64) {
+    pwLength = prompt("Your password needs to be between 10-64 characters. \n How many characters would you like in your password?");
   }
-
-  // Character Types
+  console.log(pwLength);
+  //   // Character Types
   var lcChar = confirm("Would you like to use lower case characters? \n Click OK for yes.");
   var ucChar = confirm("Would you like to use upper case characters? \n Click OK for yes.");
   var spChar = confirm("Would you like to use special characters? \n Click OK for yes.");
   var numChar = confirm("Would you like to use numeric characters? \n Click OK for yes.");
 
   // Character types validation
-  while (lcChar == false && ucChar == false && spChar == false && numChar == false) {
-    alert("You need to select at least one type of character for your password. Please try again.")
+  if (lcChar == false && ucChar == false && spChar == false && numChar == false) {
+    alert("You need to select at least one type of character for your password. Please try again.");
     var lcChar = confirm("Would you like to use lower case characters? \n Click OK for yes.");
     var ucChar = confirm("Would you like to use upper case characters? \n Click OK for yes.");
     var spChar = confirm("Would you like to use special characters? \n Click OK for yes.");
     var numChar = confirm("Would you like to use numeric characters? \n Click OK for yes.");
   }
 
-  // push chosen arrays into a new array - pwCriteria
-  
+  // if they choose ------ lowercase -------
   if (lcChar == true) {
-      pwCriteria.push(lowerCasedCharacters);
-      // lowerCasedCharacters.concat(pwArray);
+    for (let i = 0; i < +pwLength; i++) {
+      var randomLowercase = getRandom(lowerCasedCharacters);
+      pwCriteria.push(randomLowercase);
+    }
+    console.log(pwCriteria)
   }
+  // if they choose ------ uppercase -------
   if (ucChar == true) {
-      pwCriteria.push(upperCasedCharacters);
-      // upperCasedCharacters.concat(pwArray);
+    for (let i = 0; i < +pwLength; i++) {
+      var randomUppercase = getRandom(upperCasedCharacters);
+      pwCriteria.push(randomUppercase);
+    }
+    console.log(pwCriteria)
   }
+
+  // if they choose ------ special -------
   if (spChar == true) {
-      pwCriteria.push(specialCharacters);
-      // specialCharacters.concat(pwArray);
+    for (let i = 0; i < +pwLength; i++) {
+      var randomSpecial = getRandom(specialCharacters);
+      pwCriteria.push(randomSpecial);
+    }
+    console.log(pwCriteria)
   }
+  // if they choose ------ numeric -------
   if (numChar == true) {
-      pwCriteria.push(numericCharacters);
-      // specialCharacters.concat(pwArray);
+    for (let i = 0; i < +pwLength; i++) {
+      var randomNumeric = getRandom(numericCharacters);
+      pwCriteria.push(randomNumeric);
+    }
+    console.log(pwCriteria)
   }
-  console.log(pwCriteria)
 
-  // console.log(typeof pwCriteria)
-  // console.log(typeof pwLength)
-  // console.log(typeof +pwLength)
+
 }
 
+var rndmPW = [];
 +pwLength;
-var rndmPW = "";
 
-// Function for getting a random element from an array
-function getRandom() {
-// how do i generate a random order of items from the "true" arrays that are in my pwCriteria? 
-// then I want to slice based on the pwlength
-// rndmPW = rndmPW.slice(0,pwlength)
+var thePW = [];
+rndmPW = getRandom(pwCriteria)
 
-// OR: I wanted to do a for loop and access one random item from the pwCriteria object with each iteration to match the length of the password
-  for (let i = 0; i <= +pwLength; i++) {
-    // rndmPW = Math.floor(Math.random()*pwCriteria);
-    // PW += pwCriteria(rndmPW, rndmPW +1);
-    // rndmPW = Math.floor(Math.random() * pwCriteria)
-    // rndmPW = pwCriteria[Math.floor(Math.random() * pwCriteria)]
+function generateRndmPw(arr) {
+  let i = 0;
+  for (let i = 0; i < +pwLength; i++) {
+    rndmPW = getRandom(pwCriteria);
+    thePW.push(rndmPW);
   }
-  console.log(rndmPW)
 }
-
-
 
 // Function to generate password with user input
 function generatePassword() {
   getPasswordOptions()
-  getRandom()
-  return "This is the password: " + rndmPW
+  generateRndmPw(pwCriteria)
+  return "This is the password: " + thePW.join('')
 }
 
-
 // -----------------------------------------------------
+
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
 
@@ -176,7 +183,6 @@ var generateBtn = document.querySelector('#generate');
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
-
   passwordText.value = password;
 }
 
